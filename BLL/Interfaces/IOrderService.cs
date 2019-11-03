95c47ad8b8aedb851bef6ea8f3e88031d58c2beb
VictoryJaeger <erenwithlevi@gmail.com>
2019-11-04 00:18:57 +0200

    Creation of BLL, additon a references between projects,creation folders: Interfaces and Services, interface IOrderService

---
+++ BLL/Interfaces/IOrderService.cs
@@ -0,0 +1,13 @@
+﻿using DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BLL.Interfaces
+{
+    public interface IOrderService
+    {
+        void CreateOrder(Book book, DateTime startReservation);
+        IEnumerable<Book> SearchBook();
+    }
+}
